d3c045a3550d95b90c354f559b90329cbb339650
Vinaya2092000 <vinaya2srinivasan@gmail.com>
2022-08-25 21:33:46 +0530

    [Vinaya]Add: Swap Numbers

--- Problems.cs
+++ Problems.cs
@@ -10,18 +10,19 @@ namespace BasicProblems_A
     {
         public void UseCaseProblem()
         {
-            Console.WriteLine("Enter the Divident: ");
-            int divi = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the Divisor: ");
-            int divs = Convert.ToInt32(Console.ReadLine());
-
-            int quotient = divi / divs;
-            int remainder = divi % divs;
-
-            Console.WriteLine("Dividend:{0} Divisor:{1}", divi, divs);
-            Console.WriteLine("Quotient = " + quotient);
-            Console.WriteLine("Remainder = " + remainder);
-            Console.ReadLine();
+            int n1, n2, temp;
+            Console.Write("First Number : ");
+            n1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Second Number : ");
+            n2 = Convert.ToInt32(Console.ReadLine());
+            //Temp variable is used to swap numbers
+            temp = n1;
+            n1 = n2;
+            n2 = temp;
+            Console.Write("\nAfter Swapping : ");
+            Console.Write("\nFirst Number : " + n1);
+            Console.Write("\nSecond Number : " + n2);
+            Console.Read();
         }
     }
 }
