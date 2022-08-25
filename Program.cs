b6f6f35ee176b72cac3e3287e0b20f7d8b128a2e
Vinaya2092000 <vinaya2srinivasan@gmail.com>
2022-08-25 20:27:34 +0530

    Welcome to BasicProblems

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace BasicProblems_A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Problems problems = new Problems();
+            problems.UseCaseProblem();
+        }
+    }
+}
